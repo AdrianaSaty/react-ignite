f81dc3c790a6ce98be21aad5fb41f2dc8b1a4f0d
AdrianaSaty <adri-saty@hotmail.com>
2022-02-15 19:53:28 -0300

    config miragejs and axios

--- chapter-ii/dtmoney/src/App.tsx
+++ chapter-ii/dtmoney/src/App.tsx
@@ -2,11 +2,26 @@ import styled from 'styled-components';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs';
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return (
+        {
+          id: 1,
+          title: 'transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      )
+    })
+  }
+})
 
 export function App() {
   return (
