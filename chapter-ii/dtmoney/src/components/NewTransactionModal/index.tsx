54f31c8ddbf58ba54495850606c0a6e6c91f060d
AdrianaSaty <adri-saty@hotmail.com>
2022-02-16 18:47:07 -0300

    connected newTransactionModal to TransactionsContext

--- chapter-ii/dtmoney/src/components/NewTransactionModal/index.tsx
+++ chapter-ii/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -4,8 +4,9 @@ import { Container, RadioBox, TransactionTypeContainer } from './style';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -13,21 +14,26 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
+    const { createTransaction } = useContext(TransactionsContext);
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
-    const [value, setValue] = useState('');
     const [type, setType] = useState('deposit');
 
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-
-        const data = {
-            title, value, category, type
-        }
-        console.log(data)
-        api.post('/transactions', data)
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type
+        });
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
